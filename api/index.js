1e6ea1bd68d6072a18a44e97b303d4e830ed2e36
islam fouad <fouadeslam49@gmail.com>
2026-08-19 18:17:26 +0300

    Remove conflicting API entry and preserve Vercel API routes

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import app from '../server/src/app.js';
-import { connectDB } from '../server/src/config/db.js';
-
-const dbReady = connectDB();
-
-export default async function handler(req, res) {
-  await dbReady;
-  return app(req, res);
-}
