2b4408e4dbb8b2f93ed269a294c62e4af5ac7c54
islam fouad <fouadeslam49@gmail.com>
2026-08-19 18:30:32 +0300

    bbbb

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,9 @@
-import app from '../server/src/app.js';
-import { connectDB } from '../server/src/config/db.js';
-
-const dbReady = connectDB();
-
 export default async function handler(req, res) {
-  await dbReady;
+  const [{ default: app }, { connectDB }] = await Promise.all([
+    import('../server/src/app.js'),
+    import('../server/src/config/db.js'),
+  ]);
+
+  await connectDB();
   return app(req, res);
 }
